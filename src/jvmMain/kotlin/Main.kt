440be553ec6674fe82d98ab632c937325082fc89
MobinYardim <mobinyardim@gmail.com>
2022-11-23 23:23:25 +0330

    trace string in main

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,18 +1,43 @@
+import utils.toEnumList
+import utils.toText
+
 fun main() {
 
-    val nfa = NFA<Language>()
+    val s1 = State(1, "S1", false)
 
-    val s1 = nfa.addState(State(1, "S1", false))
+    val nfa = NFA<Language>(s1)
+
     val s2 = nfa.addState(State(2, "S2", false))
     val s3 = nfa.addState(State(3, "S3", true))
 
-    nfa.addEdge(s1, Language.A, s2)
-    nfa.addEdge(s2, Language.B, s3)
+    nfa.addEdge(s1, Language.a, s2)
+    nfa.addEdge(s2, Language.a, s2)
+    nfa.addEdge(s2, Language.a, s3)
 
-    println("Hello, Kotlin/Native!")
+    nfa.trace(
+        string = "aab".toEnumList(),
+        automataStateTracer = object : AutomataStateTracer<Language> {
+            override fun onStart() {
+                println("onStart")
+            }
+
+            override fun onCurrentStateChange(state: State) {
+                println("onCurrentStateChange:$state")
+            }
+
+            override fun onFinalState(state: State) {
+                println("onFinalState:$state")
+            }
+
+            override fun onTrap(state: State, notConsumedString: List<Language>) {
+                println("onTrap:$state notConsumedString:${notConsumedString.toText()}")
+            }
+
+        }
+    )
 }
 
 enum class Language {
-    A,
-    B
+    a,
+    b
 }
