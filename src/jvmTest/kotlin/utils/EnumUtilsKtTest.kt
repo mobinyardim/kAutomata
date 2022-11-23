d30af58b467dc0400b623ca1d8f9f3cca46b9186
MobinYardim <mobinyardim@gmail.com>
2022-11-23 23:58:31 +0330

    test to enum list function

--- src/jvmTest/kotlin/utils/EnumUtilsKtTest.kt
+++ src/jvmTest/kotlin/utils/EnumUtilsKtTest.kt
@@ -2,7 +2,7 @@ package utils
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
 import com.google.common.truth.Truth.assertThat
 
 internal class EnumUtilsKtTest {
@@ -27,5 +27,30 @@ internal class EnumUtilsKtTest {
         val listOfSymbols = listOf<Language>()
 
         assertThat(listOfSymbols.toText()).isEqualTo("")
+    }
+
+    @Test
+    fun `toEnumList when text is empty`() {
+        val text = ""
+
+        assertThat(text.toEnumList<Language>()).isEqualTo(listOf<Language>())
+    }
+
+    @Test
+    fun `toEnumList when text contain illegal character`() {
+        val text = "x"
+
+        assertThrows<IllegalArgumentException> {
+            text.toEnumList<Language>()
+        }
+    }
+
+    @Test
+    fun `toEnumList when text is valid`() {
+        val text = "abab"
+
+        assertThat(text.toEnumList<Language>()).isEqualTo(
+            listOf(Language.a, Language.b, Language.a, Language.b)
+        )
     }
 }
