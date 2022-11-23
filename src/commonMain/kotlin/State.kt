bf0017de7819be190275cb0689c5689be4990b90
MobinYardim <mobinyardim@gmail.com>
2022-11-23 17:11:37 +0330

    delete overrided functions

--- src/commonMain/kotlin/State.kt
+++ src/commonMain/kotlin/State.kt
@@ -2,12 +2,4 @@ data class State(
     val id: Int,
     val name: String,
     val isFinal: Boolean
-) {
-    override fun hashCode(): Int {
-        return id
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return super.equals(other)
-    }
-}
+)
