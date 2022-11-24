66dd5fe30b201d02a3120a9d79853c188f7d55d5
MobinYardim <mobinyardim@gmail.com>
2022-11-24 17:04:29 +0330

    delete mockito core implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,6 @@ kotlin {
                 implementation(kotlin("test"))
                 implementation ("com.google.truth:truth:1.1.3")
                 implementation ("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
-                implementation("org.mockito.kotlin:mockito-core:4.0.0")
                 implementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
             }
         }
