78f2c7c5f0b60be19b5714feb5b4795a7a9bee92
MobinYardim <mobinyardim@gmail.com>
2022-11-24 17:43:58 +0330

    change example to contain lambda transition

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -8,14 +8,20 @@ fun main() {
     val nfa = NFA<Language>(s1)
 
     val s2 = nfa.addState(State(2, "S2", false))
-    val s3 = nfa.addState(State(3, "S3", true))
+    val s3 = nfa.addState(State(3, "S3", false))
+    val s4 = nfa.addState(State(4, "S4", true))
+    val s5 = nfa.addState(State(5, "S5", false))
+    val s6 = nfa.addState(State(6, "S6", false))
 
-    nfa.addEdge(s1, Language.a, s2)
-    nfa.addEdge(s2, Language.a, s2)
+    nfa.addEdge(s1, null, s2)
+    nfa.addEdge(s1, null, s5)
     nfa.addEdge(s2, Language.a, s3)
+    nfa.addEdge(s3, Language.a, s4)
+    nfa.addEdge(s5, Language.b, s6)
+    nfa.addEdge(s6, Language.b, s4)
 
     nfa.trace(
-        string = "aab".toEnumList(),
+        string = "aa".toEnumList(),
         automataStateTracer = object : AutomataStateTracer<Language> {
             override fun onStart() {
                 println("onStart")
@@ -27,6 +33,10 @@ fun main() {
 
             override fun onFinalState(state: State) {
                 println("onFinalState:$state")
+            }
+
+            override fun onTransition(start: State, transition: Language?, endState: State) {
+                println("onTransition: from:$start with:${transition?.name ?: "lambda"} to:${endState}")
             }
 
             override fun onTrap(state: State, notConsumedString: List<Language>) {
