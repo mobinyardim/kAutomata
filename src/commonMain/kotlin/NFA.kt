27a6b2dc0bc75f3315ae6f26716219598e6ef664
MobinYardim <mobinyardim@gmail.com>
2022-11-24 19:00:42 +0330

    move add edge function to nfa class

--- src/commonMain/kotlin/NFA.kt
+++ src/commonMain/kotlin/NFA.kt
@@ -1,3 +1,32 @@
+import exceptions.DuplicatedEdgeException
+import exceptions.NoSuchStateException
+
 class NFA<T : Enum<T>>(
     startState: State = State(0, "s0", false)
-) : Automata<T>(startState)
+) : Automata<T>(startState) {
+    fun addEdge(startState: State, transition: T?, endState: State) {
+        if (!containsState(startState) || !containsState(endState))
+            throw NoSuchStateException()
+
+        if (containsEdge(startState, transition, endState)) {
+            throw DuplicatedEdgeException(
+                startState, transition?.name ?: "lambda", endState
+            )
+        }
+
+        val stateEdges = edges[startState]?.toMutableMap() ?: mutableMapOf()
+        val newEdges = stateEdges[transition]?.toMutableSet() ?: mutableSetOf()
+
+
+
+        newEdges.add(endState)
+        stateEdges[transition] = newEdges
+
+        if (edges[startState]?.contains(transition) == true) {
+            edges.remove(startState)
+            edges[startState] = stateEdges
+        } else {
+            edges[startState] = stateEdges
+        }
+    }
+}
