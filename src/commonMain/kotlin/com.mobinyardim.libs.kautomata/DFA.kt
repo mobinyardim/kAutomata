b34ca0845223cb6342f64a3c7d662ec70f3e4ae5
MobinYardim <mobinyardim@gmail.com>
2022-12-05 18:16:27 +0330

    return exception when duplicated edge add

--- src/commonMain/kotlin/com.mobinyardim.libs.kautomata/DFA.kt
+++ src/commonMain/kotlin/com.mobinyardim.libs.kautomata/DFA.kt
@@ -1,10 +1,18 @@
 package com.mobinyardim.libs.kautomata
 
+import com.mobinyardim.libs.kautomata.exceptions.DuplicatedEdgeException
+
 class DFA<T : Enum<T>>(
     startState: State = State(0, "s0", false)
 ) : Automata<T>(startState) {
 
     fun addEdge(startState: State, transition: T, endState: State) {
+        if (containsEdge(startState, transition, endState)) {
+            throw DuplicatedEdgeException(
+                startState, transition.name, endState
+            )
+        }
+
         removeEdge(
             startState = startState,
             transition = transition
