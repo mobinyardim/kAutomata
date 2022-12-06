04e3d58cf3a626d615811b14bc896b3d29a2f0b4
MobinYardim <mobinyardim@gmail.com>
2022-12-06 09:50:03 +0330

    remove elements after iterate

--- src/commonMain/kotlin/com.mobinyardim.libs.kautomata/utils/MapUtils.kt
+++ src/commonMain/kotlin/com.mobinyardim.libs.kautomata/utils/MapUtils.kt
@@ -4,9 +4,13 @@ package com.mobinyardim.libs.kautomata.utils
 fun <K, V> MutableMap<K, V>.removeIf(
     filter: (Map.Entry<K, V>) -> Boolean
 ) {
+    val removedList = mutableListOf<K>()
     for (entry in this) {
         if (filter(entry)) {
-            this.remove(entry.key)
+            removedList.add(entry.key)
         }
+    }
+    removedList.forEach {
+        this.remove(it)
     }
 }
