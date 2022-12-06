e4073e6c9320e62732341fba0dea22b3f792a0da
MobinYardim <mobinyardim@gmail.com>
2022-12-06 15:16:14 +0330

    fix bug that when there is multiple edge between two state just edges that in must reversed added to new list

--- src/commonMain/kotlin/com.mobinyardim.libs.kautomata/AutomataExt.kt
+++ src/commonMain/kotlin/com.mobinyardim.libs.kautomata/AutomataExt.kt
@@ -6,9 +6,12 @@ fun <T : Enum<T>> Automata<T>.reverseEdges(mustReverseEdges: Map<State, Map<Stat
         val firstState = entry.key
         entry.value.forEach {
             val reversedEdge = mustReverseEdges[firstState]?.get(it.key)
-            if (reversedEdge != null) {
+            if (reversedEdge != null && reversedEdge.isNotEmpty()) {
                 newEdges[it.key] = mutableMapOf<State, Set<T?>>().apply {
                     put(firstState, reversedEdge)
+                }
+                newEdges[firstState] = mutableMapOf<State, Set<T?>>().apply {
+                    put(it.key, it.value.filter { !reversedEdge.contains(it) }.toSet())
                 }
             } else {
                 newEdges[firstState] = mutableMapOf<State, Set<T?>>().apply {
