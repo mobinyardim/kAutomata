9d9179fa134e799a2c1a822f580d6cdd87afc8a0
MobinYardim <mobinyardim@gmail.com>
2022-12-07 10:20:48 +0330

    add new test that fails reverse edges

--- src/jvmTest/kotlin/com/mobinyardim/libs/kautomata/AutomataExtKtTest.kt
+++ src/jvmTest/kotlin/com/mobinyardim/libs/kautomata/AutomataExtKtTest.kt
@@ -1,12 +1,20 @@
 package com.mobinyardim.libs.kautomata
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class AutomataExtKtTest {
 
     companion object {
+        val stateId0 = 0
+        val stateName0 = "s0"
+        val isFinalState0 = false
+        val state0 = State(
+            id = stateId0,
+            name = stateName0,
+            isFinal = isFinalState0
+        )
+
         val stateId1 = 1
         val stateName1 = "s1"
         val isFinalState1 = false
@@ -45,6 +53,28 @@ internal class AutomataExtKtTest {
 
             return automata
         }
+
+        fun generateAutomata2(): Automata<AutomataTest.Language> {
+            val automata = object : Automata<AutomataTest.Language>(state0) {
+                fun addEdge(
+                    startState: State,
+                    transition: AutomataTest.Language?,
+                    endState: State
+                ) {
+                    _addEdge(
+                        startState, transition, endState
+                    )
+                }
+            }
+
+            automata.addState(state1)
+            automata.addState(state2)
+            automata.addEdge(state0, AutomataTest.Language.a, state1)
+            automata.addEdge(state1, AutomataTest.Language.b, state2)
+            automata.addEdge(state2, null, state0)
+
+            return automata
+        }
     }
 
 
@@ -71,6 +101,31 @@ internal class AutomataExtKtTest {
         val reversedEdges = automata.reverseEdges(
             mutableMapOf<State, Map<State, Set<AutomataTest.Language?>>>().apply {
                 put(state1, mapOf(state2 to setOf(AutomataTest.Language.a)))
+            }
+        )
+
+        assertThat(
+            reversedEdges
+        ).isEqualTo(edge)
+    }
+
+    @Test
+    fun `reverseEdges when incoming and outgoing edge of two state overlapping must all edges reversed`() {
+        val automata = generateAutomata2()
+
+        val edge = mutableMapOf<State, Map<State, Set<AutomataTest.Language?>>>().apply {
+            put(
+                state1,
+                mapOf(
+                    state0 to setOf(AutomataTest.Language.a),
+                    state2 to setOf(AutomataTest.Language.b)
+                )
+            )
+            put(state2, mapOf(state0 to setOf(null)))
+        }
+        val reversedEdges = automata.reverseEdges(
+            mutableMapOf<State, Map<State, Set<AutomataTest.Language?>>>().apply {
+                put(state0, mapOf(state1 to setOf(AutomataTest.Language.a)))
             }
         )
 
