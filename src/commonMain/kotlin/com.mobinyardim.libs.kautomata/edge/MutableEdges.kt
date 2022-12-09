adb3032cc999962b6ba3ed779d44c59c6451cc8f
MobinYardim <mobinyardim@gmail.com>
2022-12-09 20:57:23 +0330

    separate mutable edges to another file

---
+++ src/commonMain/kotlin/com.mobinyardim.libs.kautomata/edge/MutableEdges.kt
@@ -0,0 +1,67 @@
+package com.mobinyardim.libs.kautomata.edge
+
+import com.mobinyardim.libs.kautomata.exceptions.DuplicatedEdgeException
+
+class MutableEdges<T : Enum<T>> : Edges<T>() {
+
+    fun addEdge(edge: Edge<T>) {
+        if (_edges.contains(edge)) {
+            throw DuplicatedEdgeException(
+                edge.start, edge.transition?.name ?: "lambda", edge.end
+            )
+        }
+        _edges.add(edge)
+    }
+
+    fun removeEdge(edge: Edge<T>) {
+        if (_edges.contains(edge)) {
+            _edges.remove(edge)
+        }
+    }
+
+    fun toEdge(): Edges<T> {
+        return copy()
+    }
+
+    operator fun plusAssign(edges: Edges<T>) {
+        edges.edges.forEach {
+            if (!contain(it)) {
+                addEdge(it)
+            }
+        }
+    }
+
+    operator fun minusAssign(edges: Edges<T>) {
+        edges.edges.forEach {
+            if (contain(it)) {
+                removeEdge(it)
+            }
+        }
+    }
+
+    operator fun plusAssign(edge: Edge<T>) {
+        addEdge(edge)
+    }
+
+    operator fun minusAssign(edge: Edge<T>) {
+        addEdge(edge)
+    }
+
+    override fun hashCode(): Int {
+        return _edges.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Edges<*> -> {
+                _edges == other.edges
+            }
+            is MutableEdges<*> -> {
+                _edges == other._edges
+            }
+            else -> {
+                super.equals(other)
+            }
+        }
+    }
+}
